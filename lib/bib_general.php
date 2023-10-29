f647fbac50cf5896294928a95907022ffc1f3442
DEV <DEV@DEV-PC.lan>
2023-10-29 09:25:08 +0100

    Example pour envoyer des mails : sendmail.php

--- lib/bib_general.php
+++ lib/bib_general.php
@@ -17,4 +17,17 @@ class GET{
             return null;
     }
 }
+
+function verifMail ($mail)
+{
+    if (preg_match ('/^[a-zA-Z0-9_]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$]/i', $mail ) ) {
+        return false;
+    }
+    list ($nom, $domaine) = explode ('@', $mail);
+    if (getmxrr ($domaine, $mxhosts))  {
+        return true;
+    } else {
+        return false;
+    }
+} 
 ?>
